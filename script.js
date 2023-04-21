08606b90f68bd63b6211510b9909e9f9e0590d39
João Paulo <joaopaulo@MacBook-Air-2.local>
2023-04-21 19:14:20 -0300

    Word.js folder fixed

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const script = document.createElement('script');
 script.src = 'words.js';
 document.head.appendChild(script);
 
-// Reference the arrays of verbs and subjects from the words.js file
-const verbs = window.verbs;
-const subjects = window.subjects;
-
 function generateRandomVerb() {
   // get a random index
   const randomIndex = Math.floor(Math.random() * verbs.length);
